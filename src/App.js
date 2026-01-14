a57ff8e9ee86a80e5192e8a0c36ae834e59c5f80
nguetcheu <dnguetcheu@gmail.com>
2026-01-14 20:08:09 +0100

    Ajout du composant MovieGrid pour afficher le nombre de films

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import MovieGrid from "./components/MovieGrid";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header></Header>
+        <MovieGrid></MovieGrid>
       </div>
       <Footer></Footer>
     </div>
---
+++ src/components/MovieGrid.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import "../styles.css";
+
+export default function MovieGrid() {
+  const [movies, setMovies] = useState([]); // création du composant MovieGrid
+
+  useEffect(() => {
+    fetch("movies.json") // récupération des données depuis le fichier movies.json
+      .then((response) => response.json()) // conversion de la réponse en JSON
+      .then((data) => setMovies(data)); // mise à jour de l'état avec les données récupérées
+  }, []);
+
+  return <div>{movies.length}</div>;
+}
